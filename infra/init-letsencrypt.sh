af8d22f175b97cce8eeb19d5d363058e51acd305
Egor Remezov <mr.drode@gmail.com>
2022-11-05 20:02:14 +0300

    fix compose and ssl

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(eremezov.ru)
+domains=(yamdbv1ceo.sytes.net)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="agrobobrov@ya.ru" # Adding a valid address is strongly recommended
+email="v1ceotest@mail.ru"
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
@@ -30,19 +30,6 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
-
-
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
-
-echo "### Deleting dummy certificate for $domains ..."
 docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
